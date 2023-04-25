bc5b155df4fc651ea2a1c6a90274daf351f32607
Rajesh kumar <rk129479@gmail.com>
2023-04-25 23:35:03 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    // sticky navbar on scroll script
+    // sticky nav-menu on scroll script
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $(".nav-menu").addClass("sticky");
     } else {
-      $(".navbar").removeClass("sticky");
+      $(".nav-menu").removeClass("sticky");
     }
 
     // scroll-up button show/hide script
@@ -22,14 +22,14 @@ $(document).ready(function () {
     $("html").css("scrollBehavior", "auto");
   });
 
-  $(".navbar .menu li a").click(function () {
+  $(".nav-menu .menu li a").click(function () {
     // applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
   });
 
-  // toggle menu/navbar script
+  // toggle menu/nav-menu script
   $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
+    $(".nav-menu .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
 
