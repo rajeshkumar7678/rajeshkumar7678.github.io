5062f6951a6e762a9a04ef822fb021a2ecf403e4
Rajesh kumar <rk129479@gmail.com>
2023-04-29 23:02:11 +0530

    done

--- index.js
+++ index.js
@@ -1,80 +1,84 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    // sticky nav-menu on scroll script
-    if (this.scrollY > 20) {
-      $(".nav-menu").addClass("sticky");
-    } else {
-      $(".nav-menu").removeClass("sticky");
+// *=============================== toggle icon navbar ==================================
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('#nav-menu')
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active')
+};
+
+// *=============================== github calender ==================================
+    GitHubCalendar(".calendar", "rajeshkumar7678");
+
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "rajeshkumar7678", { responsive: true });
+
+// *=============================== resume open in new tab ==================================
+
+
+    let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
+    let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
+
+    function NewTab() {
+      window.open(
+        "https://drive.google.com/file/d/1wqVJANRNOQ0r1ClyHwGO0vOaf-ZDhfBR/view?usp=sharing",
+        "_blank"
+      );
     }
 
-    // scroll-up button show/hide script
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
-  });
+// *=============================== scroll sections active link ==================================
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a')
 
-  // slide-up script
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    // removing smooth scroll on slide-up button click
-    $("html").css("scrollBehavior", "auto");
-  });
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-  $(".nav-menu .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
-  });
+    if(top >= offset && top < offset + height) {
+        navlinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+        });
+    };
 
-  // toggle menu/nav-menu script
-  $(".menu-btn").click(function () {
-    $(".nav-menu .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
   });
-
-  // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Back-End Developer", "Believer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  var typed = new Typed(".typing-2", {
-    strings: ["Back-End Developer", "Believer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-  loop: false,
-    autoplay: false,   //for carousel effect turn it true
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
-});
-function submitForm() {
+  // *=============================== sticky navbar ==================================
    
-   var frm = document.getElementById('my_form');
-   frm.submit(); // Submit the form
-   frm.reset();  // Reset all form data
-   return false; // Prevent page refresh
-}
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+// *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active')
+
+};
+
+// *=============================== scroll reveal ==================================
+
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin:'top'});
+ScrollReveal().reveal('.home-img, .skills-container, .project-card, .contact form', { origin:'bottom'});
+ScrollReveal().reveal('.home-content h1', { origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin:'left'});
+
+// *===============================  typed js==================================
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Full Stack Web Developer', 'Backend Developer', 'Frontend Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backdelay: 1000,
+  loop: true
+})
